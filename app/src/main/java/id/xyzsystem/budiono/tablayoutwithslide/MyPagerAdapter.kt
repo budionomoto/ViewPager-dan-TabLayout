c5a5c73fcd7a7a30025c93cd38716b991edcb886
budionomoto <budionomotoe3@gmail.com>
2018-11-25 23:59:12 +0700

    Project ViewPager dan TabLayout

---
+++ app/src/main/java/id/xyzsystem/budiono/tablayoutwithslide/MyPagerAdapter.kt
@@ -0,0 +1,33 @@
+package id.xyzsystem.budiono.tablayoutwithslide
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm){
+
+    // sebuah list yang menampung objek Fragment
+    private val pages = listOf(
+        FirstFragment(),
+        SecondFragment(),
+        ThridFragment()
+    )
+
+    // menentukan fragment yang akan dibuka pada posisi tertentu
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    // judul untuk tabs
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "First Tab"
+            1 -> "Second Tab"
+            else -> "Third Tab"
+        }
+    }
+}
